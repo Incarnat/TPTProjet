d47830dc1ef4ff3bcfddf991630070a7266fba19
noret <noret@c124-10.enst.fr>
2014-06-16 09:31:50 +0200

    lol

--- TPTserver/src/utilisateur.h
+++ TPTserver/src/utilisateur.h
@@ -2,12 +2,14 @@
 #define UTILISATEUR_H
 
 #include <string>
-
+#include <odb/core.hxx>
 using namespace std;
 
+#pragma db object
 class Utilisateur
 {
 protected:
+#pragma db id auto
     int id;
     string type;
     string marqueMachine;
