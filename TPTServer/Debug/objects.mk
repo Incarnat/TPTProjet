b358525010635705132d6c80b184532607512b2f
noret <noret@c130-23.enst.fr>
2014-06-18 14:37:10 +0200

    dunnowut

--- TPTServer/Debug/objects.mk
+++ TPTServer/Debug/objects.mk
@@ -2,7 +2,7 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-USER_OBJS :=   /cal/homes/noret/git/TPTServer/TPTServer/utilisateur-odb.o
+USER_OBJS := /cal/homes/noret/git/TPTServer/TPTServer/point-odb.o /cal/homes/noret/git/TPTServer/TPTServer/mouvement-odb.o /cal/homes/noret/git/TPTServer/TPTServer/utilisateur-odb.o
 
 LIBS := -lpthread -lboost_system -lodb -lodb-mysql
 
