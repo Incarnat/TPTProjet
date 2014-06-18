bbcd000d0366963343620a693b1461a9d298e74a
kevintouchpad <guidez.kevin@gmail.com>
2014-06-18 15:38:32 +0200

    Update mouvement.cpp

--- TPTServer/src/mouvement.cpp
+++ TPTServer/src/mouvement.cpp
@@ -1,5 +1,22 @@
-#include "mouvement.h"
+#ifndef MOUVEMENT_H
+#define MOUVEMENT_H
 
-Mouvement::Mouvement()
+#include <string>
+
+using namespace std;
+
+class Mouvement
 {
-}
+protected :
+    int idMove;
+    int idUser;
+    string type;
+    string application;
+
+public:
+    Mouvement();
+    Mouvement(string _type, string _application);
+    int getIdMove();
+};
+
+#endif // MOUVEMENT_H
